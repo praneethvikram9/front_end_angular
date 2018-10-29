b517c25dba0c98f906fbf801007d5b960d1cd9ba
CGI-Praneeth <praneethvikram635@gmail.com>
2018-10-29 10:29:49 +0530

    new updates

--- Videos/Angular/movie/src/app/movie.service.ts
+++ Videos/Angular/movie/src/app/movie.service.ts
@@ -10,9 +10,9 @@ export class MovieService {
 
   public url = "https://api.themoviedb.org/3/movie/upcoming?api_key=a11a51e2627becc9456d9ef4db35443f";
 
-  public url2 ="http://localhost:8099/api/v1/movie/add";
-  public url3 ="http://localhost:8099/api/v1/movie/show";
-  public url4="http://localhost:8099/api/v1/movie"
+  public url2 ="http://localhost:8098/api/v1/movie/add";
+  public url3 ="http://localhost:8098/api/v1/movie/show";
+  public url4="http://localhost:8098/api/v1/movie";
   constructor(private http: HttpClient) { }
 
   getMovies():Observable<any>{
